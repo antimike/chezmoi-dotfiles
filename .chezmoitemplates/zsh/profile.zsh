91af69b63d9fd8b8971513f7c19c8358c54eca7c
Michael Haynes <antihaynes@gmail.com>
2023-11-26 17:29:14 -0600

    Template .zshrc and .zshenv

---
+++ .chezmoitemplates/zsh/profile.zsh
@@ -0,0 +1,12 @@
+{{ if .zsh.profile_startup }}
+# - - - - - - - - - - - - - - - - - - - -
+# End Profiling Script
+# - - - - - - - - - - - - - - - - - - - -
+
+if [[ "$PROFILE_STARTUP" == true ]]; then
+    unsetopt xtrace
+    exec 2>&3 3>&-
+    zprof > ~/zshprofile$(date +'%s')
+fi
+{{ end }}
+
---
+++ .chezmoitemplates/zsh/set_options.zsh
@@ -0,0 +1,37 @@
+autoload -U colors && colors    # Load Colors.
+unsetopt case_glob              # Use Case-Insensitve Globbing.
+setopt globdots                 # Glob Dotfiles As Well.
+setopt extendedglob             # Use Extended Globbing.
+setopt autocd                   # Automatically Change Directory If A Directory Is Entered.
+
+# Smart URLs.
+autoload -Uz url-quote-magic
+zle -N self-insert url-quote-magic
+
+# General.
+setopt brace_ccl                # Allow Brace Character Class List Expansion.
+setopt combining_chars          # Combine Zero-Length Punctuation Characters ( Accents ) With The Base Character.
+setopt rc_quotes                # Allow 'Henry''s Garage' instead of 'Henry'\''s Garage'.
+unsetopt mail_warning           # Don't Print A Warning Message If A Mail File Has Been Accessed.
+
+# Jobs.
+setopt long_list_jobs           # List Jobs In The Long Format By Default.
+setopt auto_resume              # Attempt To Resume Existing Job Before Creating A New Process.
+setopt notify                   # Report Status Of Background Jobs Immediately.
+unsetopt bg_nice                # Don't Run All Background Jobs At A Lower Priority.
+unsetopt hup                    # Don't Kill Jobs On Shell Exit.
+unsetopt check_jobs             # Don't Report On Jobs When Shell Exit.
+
+setopt correct                  # Turn On Corrections
+
+# Completion Options.
+setopt complete_in_word         # Complete From Both Ends Of A Word.
+setopt always_to_end            # Move Cursor To The End Of A Completed Word.
+setopt path_dirs                # Perform Path Search Even On Command Names With Slashes.
+setopt auto_menu                # Show Completion Menu On A Successive Tab Press.
+setopt auto_list                # Automatically List Choices On Ambiguous Completion.
+setopt auto_param_slash         # If Completed Parameter Is A Directory, Add A Trailing Slash.
+setopt no_complete_aliases
+
+setopt menu_complete            # Do Not Autoselect The First Completion Entry.
+unsetopt flow_control           # Disable Start/Stop Characters In Shell Editor.
